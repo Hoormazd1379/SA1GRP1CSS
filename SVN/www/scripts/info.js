0ed47175e58fe6212634f8aefc7f752598f16e28
Hoormazd Pirayeshfar <42213001+Hoormazd1379@users.noreply.github.com>
2019-11-07 16:42:55 +0100

    SVN Update rev 202

--- SVN/www/scripts/info.js
+++ SVN/www/scripts/info.js
@@ -1,9 +1,61 @@
-/* JQuery */
+/* JQuery:
+
+Scroll library for the homepage by: Hoormazd Pirayeshfar
+
+ */
 $(document).scroll(function() {
   var y = $(this).scrollTop();
-  if (y > 800) {
-    $('.infoPage').fadeIn(1000);
-  } else {
+
+  /* VUL*/
+  if (y > 1000 && y < 2000) {
+    $('#infoVUL').fadeIn(1000);
+    $('#infoMORE').fadeOut();
+    $('#infoHIS').fadeOut();
+    $('#infoBUG').fadeOut();
+    $('#infoMAL').fadeOut();
+  } 
+
+  /* MAL*/
+  else if (y > 2000 && y < 3000) {
+    $('#infoMAL').fadeIn(1000);
+    $('#infoMORE').fadeOut();
+    $('#infoHIS').fadeOut();
+    $('#infoBUG').fadeOut();
+    $('#infoVUL').fadeOut();
+  } 
+
+  /* BUG*/
+  else if (y > 3000 && y < 4000) {
+    $('#infoBUG').fadeIn(1000);
+    $('#infoMORE').fadeOut();
+    $('#infoHIS').fadeOut();
+    $('#infoMAL').fadeOut();
+    $('#infoVUL').fadeOut();
+  } 
+
+  /* HIS*/
+  else if (y > 4000 && y < 5000) {
+    $('#infoHIS').fadeIn(1000);
+    $('#infoMORE').fadeOut();
+    $('#infoBUG').fadeOut();
+    $('#infoMAL').fadeOut();
+    $('#infoVUL').fadeOut();
+  } 
+
+  /* MORE*/
+  else if (y > 5000 && y < 6000) {
+    $('#infoMORE').fadeIn(1000);
+    $('#infoHIS').fadeOut();
+    $('#infoBUG').fadeOut();
+    $('#infoMAL').fadeOut();
+    $('#infoVUL').fadeOut();
+  } 
+
+  else if (y > 6000) {
+    $(this).scrollTop(0);
+  }
+
+  else {
     $('.infoPage').fadeOut();
   }
 });
