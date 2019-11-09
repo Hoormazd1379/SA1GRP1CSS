b69c16561eb8bbc8425ca48e70ca3d26fbdf957a
Hoormazd Pirayeshfar <42213001+Hoormazd1379@users.noreply.github.com>
2019-11-09 11:57:49 +0100

    SVN Update 349

--- SVN/www/scripts/his.js
+++ SVN/www/scripts/his.js
@@ -1,14 +1,22 @@
 // handler for his page.
 // Author: Edoardo Riggio
 
-function n1(){
+function milestones(id, numb){
     elements = document.getElementsByClassName('milestone');
     for (var i = 0; i < elements.length; i++) {
         elements[i].style.backgroundColor="#545454";
     }
 
-    document.getElementById('n1').style.background = "#C3073F";
-    document.getElementsByClassName('date-hover-mil')[0].style.display = 'block'
+    document.getElementById(id).style.backgroundColor = "#C3073F";
+
+    hover_str = document.getElementsByClassName('date-hover-mil');
+    for (var i = 0; i < hover_str.length; i++) {
+        if (i == numb){
+            hover_str[numb].style.display = 'block';
+        } else {
+            hover_str[i].style.display = 'none';
+        }
+    }
     
     sub_cat = document.getElementsByClassName('normal');
     for (var i = 0; i < sub_cat.length; i++) {
@@ -16,6 +24,35 @@ function n1(){
     }
 }
 
-function n2 (){
-    
+function sbuCat(id, numb){
+    elements = document.getElementsByClassName('milestone');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor="#545454";
+    }
+
+    document.getElementById(id).style.backgroundColor = "#C3073F";
+
+    hover_str = document.getElementsByClassName('date-hover-mil');
+    for (var i = 0; i < hover_str.length; i++) {
+        if (i == numb){
+            hover_str[numb].style.display = 'block';
+        } else {
+            hover_str[i].style.display = 'none';
+        }
+    }
+
+    sub_cat = document.getElementsByClassName('normal');
+    for (var i = 0; i < sub_cat.length; i++) {
+        sub_cat[i].style.display = 'none';
+    }
+}
+
+function showSub1(){
+    sub_cat = document.getElementsByClassName('normal');
+    sub_cat['n2.1'].style.display = 'block';
+    sub_cat['n2.2'].style.display = 'block';
+}
+
+function onMouse(id){
+    document.getElementById(id).style.backgroundColor = '#C3073F'
 }
