e699ffdb7b5ead2be3062592d94088a15ea1763d
Hoormazd Pirayeshfar <42213001+Hoormazd1379@users.noreply.github.com>
2019-11-09 13:06:18 +0100

    SVN Update 355

--- SVN/www/scripts/his.js
+++ SVN/www/scripts/his.js
@@ -1,6 +1,8 @@
 // handler for his page.
 // Author: Edoardo Riggio
 
+var selected = -1
+
 function milestones(id, numb){
     elements = document.getElementsByClassName('milestone');
     for (var i = 0; i < elements.length; i++) {
@@ -9,7 +11,7 @@ function milestones(id, numb){
 
     document.getElementById(id).style.backgroundColor = "#C3073F";
 
-    hover_str = document.getElementsByClassName('date-hover-mil');
+    hover_str = document.getElementsByClassName('date-mil');
     for (var i = 0; i < hover_str.length; i++) {
         if (i == numb){
             hover_str[numb].style.display = 'block';
@@ -22,6 +24,8 @@ function milestones(id, numb){
     for (var i = 0; i < sub_cat.length; i++) {
         sub_cat[i].style.display = 'none';
     }
+
+    selected = numb
 }
 
 function sbuCat(id, numb){
@@ -53,6 +57,69 @@ function showSub1(){
     sub_cat['n2.2'].style.display = 'block';
 }
 
-function onMouse(id){
-    document.getElementById(id).style.backgroundColor = '#C3073F'
+document.getElementById('n1').onmouseover = onMouse;
+document.getElementById('n1').onmouseleave = leaveMouse1;
+document.getElementById('n2').onmouseover = onMouse2;
+document.getElementById('n2').onmouseleave = leaveMouse2;
+document.getElementById('n3').onmouseover = onMouse3;
+document.getElementById('n3').onmouseleave = leaveMouse3;
+
+// Actions for n1
+
+function onMouse(){
+    document.getElementById('n1').style.backgroundColor = '#C3073F'
+
+    hover_str = document.getElementsByClassName('date-mil');
+    hover_str[0].style.display = "block"
+}
+
+function leaveMouse1(){
+    if (selected == 0) {
+        document.getElementById('n1').style.backgroundColor = '#C3073F'
+    } else {
+        document.getElementById('n1').style.backgroundColor = '#535353'
+        
+        hover_str = document.getElementsByClassName('date-mil');
+        hover_str[0].style.display = "none"
+    }
+}
+
+// Actions for n2
+
+function onMouse2(){
+    document.getElementById('n2').style.backgroundColor = '#C3073F'
+
+    hover_str = document.getElementsByClassName('date-mil');
+    hover_str[1].style.display = "block"
+}
+
+function leaveMouse2(){
+    if (selected == 1) {
+        document.getElementById('n2').style.backgroundColor = '#C3073F'
+    } else {
+        document.getElementById('n2').style.backgroundColor = '#535353'
+
+        hover_str = document.getElementsByClassName('date-mil');
+        hover_str[1].style.display = "none"
+    }
+}
+
+// Actions for n3
+
+function onMouse3(){
+    document.getElementById('n3').style.backgroundColor = '#C3073F'
+
+    hover_str = document.getElementsByClassName('date-mil');
+    hover_str[2].style.display = "block"
+}
+
+function leaveMouse3(){
+    if (selected == 2) {
+        document.getElementById('n3').style.backgroundColor = '#C3073F'
+    } else {
+        document.getElementById('n3').style.backgroundColor = '#535353'
+
+        hover_str = document.getElementsByClassName('date-mil');
+        hover_str[2].style.display = "none"
+    }
 }
